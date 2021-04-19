a228e753aebdc09d442561f10510c86d5b28e1f5
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-04-18 23:22:56 -0500

    Implemented model in form and show predictions 😎

--- demo/ai/model.py
+++ demo/ai/model.py
@@ -3,36 +3,118 @@ import pandas as pd
 import joblib
 
 
+class ModelData():
+    def __init__(self, area=0, banos=0, estrato=0, garajes=0, habitaciones=0, piso=0, valoradministracion=0, valorventa=0, latitud=0, longitud=0, anos_0_1=0, anos_1_10=0, anos_10_30=0, anos_30=0, tiponegocio_Arriendo=0, tiponegocio_Venta=0, tiponegocio_Venta_Y_Arriendo=0, tipoinmueble_Casa=0, tipoinmueble_Apartamento=0):
+        self.area = area
+        self.banos = banos
+        self.estrato = estrato
+        self.garajes = garajes
+        self.habitaciones = habitaciones
+        self.piso = piso
+        self.valoradministracion = valoradministracion
+        self.valorventa = valorventa
+        self.latitud = latitud
+        self.longitud = longitud
+        self.anos_0_1 = anos_0_1
+        self.anos_1_10 = anos_1_10
+        self.anos_10_30 = anos_10_30
+        self.anos_30 = anos_30
+        self.tiponegocio_Arriendo = tiponegocio_Arriendo
+        self.tiponegocio_Venta = tiponegocio_Venta
+        self.tiponegocio_Venta_Y_Arriendo = tiponegocio_Venta_Y_Arriendo
+        self.tipoinmueble_Casa = tipoinmueble_Casa
+        self.tipoinmueble_Apartamento = tipoinmueble_Apartamento
+
+    def to_arr(self):
+        return [
+            self.area,
+            self.banos,
+            self.estrato,
+            self.garajes,
+            self.habitaciones,
+            self.piso,
+            self.valoradministracion,
+            self.valorventa,
+            self.latitud,
+            self.longitud,
+            self.anos_0_1,
+            self.anos_1_10,
+            self.anos_10_30,
+            self.anos_30,
+            self.tiponegocio_Arriendo,
+            self.tiponegocio_Venta,
+            self.tiponegocio_Venta_Y_Arriendo,
+            self.tipoinmueble_Casa,
+            self.tipoinmueble_Apartamento,
+        ]
+
+    def __str__(self):
+        return f'''
+        ###################################################################################
+        ###################################################################################
+        MODEL DATA: 
+        area: {self.area}
+        banos: {self.banos}
+        estrato: {self.estrato}
+        garajes: {self.garajes}
+        habitaciones: {self.habitaciones}
+        piso: {self.piso}
+        valoradministracion: {self.valoradministracion}
+        valorventa: {self.valorventa}
+        latitud: {self.latitud}
+        longitud: {self.longitud}
+        anos_0_1: {self.anos_0_1}
+        anos_1_10: {self.anos_1_10}
+        anos_10_30: {self.anos_10_30}
+        anos_30: {self.anos_30}
+        tiponegocio_Arriendo: {self.tiponegocio_Arriendo}
+        tiponegocio_Venta: {self.tiponegocio_Venta}
+        tiponegocio_Venta_Y_Arri: {self.tiponegocio_Venta_Y_Arriendo}
+        tipoinmueble_Casa: {self.tipoinmueble_Casa}
+        tipoinmueble_Apartamento: {self.tipoinmueble_Apartamento}
+        ###################################################################################
+        ###################################################################################
+        '''
+
+
 class XGBoost:
     def __init__(self):
         self.xgBoost = XGBRegressor()
-        self.cols = ['area', 'banos', 'estrato', 'garajes', 'habitaciones', 'piso',
-                     'valoradministracion', 'valorventa', 'latitud', 'longitud', '0-1 anos', '1-10 anos',
-                     '10-30 anos', '30 anos', 'tiponegocio_Arriendo', 'tiponegocio_Venta',
-                     'tiponegocio_Venta Y Arriendo', 'tipoinmueble_Casa',
-                     'tipoinmueble_Apartamento'
-                     ]
+        self.cols = [
+            'area', 'banos', 'estrato', 'garajes', 'habitaciones', 'piso',
+            'valoradministracion', 'valorventa', 'latitud', 'longitud', '0-1 anos', '1-10 anos',
+            '10-30 anos', '30 anos', 'tiponegocio_Arriendo', 'tiponegocio_Venta',
+            'tiponegocio_Venta Y Arriendo', 'tipoinmueble_Casa',
+            'tipoinmueble_Apartamento'
+        ]
 
-    def load_model(self, path='./sklearn_pipeline.pkl'):
+    def load_model(self, path='./ai/sklearn_pipeline.pkl'):
         self.xgBoost = joblib.load(path)
 
     def predict(self, arr: list) -> list:
         df = pd.DataFrame(data=arr).T
         df.columns = self.cols
-        print(df)
         return self.xgBoost.predict(df)
 
 
 if __name__ == '__main__':
     model = XGBoost()
     model.load_model()
-    res = model.predict(
-        [
-            104, 2, 2, 1,
-            3, 3, 0, 185000000,
-            4.71150, - 74.13238, 0, 0,
-            0, 0, 0, 1,
-            0, 1, 0
-        ]
+    data = ModelData(
+        104, 2, 2, 1,
+        3, 3, 0, 185000000,
+        4.71150, - 74.13238, 0, 0,
+        0, 0, 0, 1,
+        0, 1, 0
     )
+    res = model.predict(data.to_arr())
+    # res = model.predict(
+    #     [
+    #         104, 2, 2, 1,
+    #         3, 3, 0, 185000000,
+    #         4.71150, - 74.13238, 0, 0,
+    #         0, 0, 0, 1,
+    #         0, 1, 0
+    #     ]
+    # )
     print(res)
--- demo/screens/demo.py
+++ demo/screens/demo.py
@@ -1,7 +1,7 @@
+from ai.model import ModelData, XGBoost
 from geo_data import GeoLocation
 import streamlit as st
 import pandas as pd
-import geopy
 
 
 geoClient = GeoLocation()
@@ -23,16 +23,25 @@ def demo():
     col1, col2 = st.beta_columns(2)
     area = col1.number_input(
         '¿Cuál es el área de tu inmueble?', min_value=0, step=1, format='%d')
-    banos = col2.number_input('¿Cuántos baños tiene?',
-                              min_value=0,  step=1, format='%d')
+    banos = col2.number_input(
+        '¿Cuántos baños tiene?',
+        min_value=0,  step=1, format='%d')
     garajes = col1.number_input(
         '¿Cuántos garajes tiene tu inmueble?', min_value=0,  step=1, format='%d')
     habitaciones = col2.number_input(
         '¿Cuántas habitaciones tiene?', min_value=0,  step=1, format='%d')
     piso = col1.number_input(
         '¿Cuántos pisos tiene tu casa?', min_value=0,  step=1, format='%d')
+    estrato = col1.number_input(
+        '¿Cual es el estrato? (Valor minimo: 1)', min_value=1,
+        step=1, format='%d'
+    )
     valoradministracion = col2.number_input(
         '¿Cuánto pagas de administración? (Si no aplica deja en 0)', min_value=0,
+        step=1, format='%d'
+    )
+    valorVivienda = col2.number_input(
+        '¿Cuánto crees que cuesta tu casa?', min_value=0,
         step=1, format='%d'
     )
 
@@ -45,12 +54,52 @@ def demo():
     lon = location.lng
     map_data = pd.DataFrame({'lat': [lat], 'lon': [lon]})
     st.map(map_data, zoom=14)
-    tiempo_de_construido = st.slider('¿Cuánto años tiene de construido tu inmueble?',
-                                     min_value=0,
-                                     max_value=30,
-                                     value=0,
-                                     step=1)
+    tiempo_de_construido = st.slider(
+        '¿Cuánto años tiene de construido tu inmueble?', min_value=0, max_value=30, value=0, step=1
+    )
     tipo_negocio = st.radio('¿Qué tipo de negocio se puede haer con la inmueble?',
                             ('Sólo Venta', 'Arriendo y Venta', 'Sólo Arriendo'))
     tipo_inmueble = st.radio(
-        '¿Qué tipo de inmueble es?', ('Casa', 'Apartamento'))
+        '¿Qué tipo de inmueble es?', ('Casa', 'Apartamento')
+    )
+
+    if st.button('Predecir'):
+        data = ModelData(
+            area=area,
+            banos=banos,
+            garajes=garajes,
+            habitaciones=habitaciones,
+            piso=piso,
+            estrato=estrato,
+            valoradministracion=valoradministracion,
+            valorventa=valorVivienda,
+            latitud=lat,
+            longitud=lon,
+        )
+        if tipo_inmueble == 'Casa':
+            data.tipoinmueble_Casa = 1
+        else:
+            data.tipoinmueble_Apartamento = 1
+
+        if tiempo_de_construido <= 1:
+            data.anos_0_1 = 1
+        elif tiempo_de_construido <= 10:
+            data.anos_1_10 = 1
+        elif tiempo_de_construido <= 30:
+            data.anos_10_30 = 1
+        else:
+            data.anos_30 = 1
+
+        if tipo_negocio == 'Sólo Venta':
+            data.tiponegocio_Venta = 1
+        elif tipo_negocio == 'Arriendo y Venta':
+            data.tiponegocio_Venta_Y_Arri = 1
+        else:
+            data.tiponegocio_Arriendo = 1
+        print(data)
+        with st.spinner('Prediciendo...'):
+            model = XGBoost()
+            model.load_model()
+            predictions = model.predict(data.to_arr())
+
+        st.success(f'El precio por metro cuadrado es de: {predictions[0]}')
